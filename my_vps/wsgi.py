1a481fb9d52ca7827323c65ebc5c0e8a071a6978
Oleksandr Danylchenko <sanekm2010@gmail.com>
2022-07-01 19:47:03 +0300

    Setting up

--- my_vps/wsgi.py
+++ my_vps/wsgi.py
@@ -7,10 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append("/var/www/my_vps")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_vps.settings')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'my_vps.settings'
 
 application = get_wsgi_application()
