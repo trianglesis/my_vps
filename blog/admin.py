ac63ecf642c4299afbd05065e0eb1926b5b6ca49
Oleksandr Danylchenko <sanekm2010@gmail.com>
2022-07-02 19:35:42 +0300

    New app for blog Debug model views

---
+++ blog/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from blog.models import Post
+
+# Register your models here.
+# admin.site.unregister(Post)
+
+admin.site.register(Post)
--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from main.models import *
 
 # Register your models here.
+# admin.site.unregister(User)
+
+# DEBUG:
+# admin.site.register(User)
+# admin.site.register(AuthGroup)
+# admin.site.register(AuthGroupPermissions)
+# admin.site.register(AuthPermission)
+# admin.site.register(AuthUser)
+# admin.site.register(AuthUserGroups)
+# admin.site.register(AuthUserUserPermissions)
+# admin.site.register(DjangoAdminLog)
+# admin.site.register(DjangoContentType)
+# admin.site.register(DjangoMigrations)
+# admin.site.register(DjangoSession)
