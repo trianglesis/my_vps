f668355f2d75250c684a85c9f38e690379e5ae9c
Oleksandr Danylchenko <sanekm2010@gmail.com>
2022-07-04 15:11:48 +0300

    Fix JS path static

--- static/remotes/js/cam-refresh.js
+++ static/remotes/js/cam-refresh.js
@@ -6,11 +6,22 @@ $(document).ready(function () {
 function runContinuously() {
     console.log("Refreshing camera views");
     let cameras = document.getElementsByClassName('cam-refresh');
+    let cam_card_datetime = document.getElementsByClassName('current-time');
     for (let cam of cameras) {
         replaceCamView(cam)
+    }
+    for (let card_txt of cam_card_datetime) {
+        currentDateTime(card_txt)
     }
 }
 
 function replaceCamView(camObj) {
     camObj.src = camObj.src + "&" + Date.now();
+}
+
+function currentDateTime(camCard) {
+    let today = new Date();
+    camCard.textContent = ''
+    camCard.textContent = today.toLocaleTimeString();
+
 }
