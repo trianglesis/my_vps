9fe2360bd7c38162bc04d966f8c53603c0fce837
Oleksandr Danylchenko <sanekm2010@gmail.com>
2022-07-04 14:10:04 +0300

    Remotes add

--- remotes/urls.py
+++ remotes/urls.py
@@ -5,5 +5,8 @@ from remotes.views import *
 urlpatterns = [
     # Home
     path('', MainPageRemotes.as_view(), name='remotes'),
+    path('mobile', RemotesMobile.as_view(), name='remotes_mobile'),
+    path('web', RemotesWeb.as_view(), name='remotes_web'),
+    path('cams', RemotesAllCameras.as_view(), name='remotes_cams'),
     # path('remotes/', MainPageRemotes.as_view(), name='remotes'),
 ]
--- remotes/views.py
+++ remotes/views.py
@@ -22,5 +22,47 @@ class MainPageRemotes(TemplateView):
         )
         return context
 
-    # Do not show anything if user is not from family group.
 
+@method_decorator(login_required, name='dispatch')
+class RemotesMobile(TemplateView):
+    template_name = 'mobile/mobile.html'
+    context_object_name = 'objects'
+
+    def get_context_data(self, **kwargs):
+        context = super(RemotesMobile, self).get_context_data(**kwargs)
+        title = 'Cameras and buttons mobile'
+        context.update(
+            title=title,
+            content='Here show and choose some modules'
+        )
+        return context
+
+
+@method_decorator(login_required, name='dispatch')
+class RemotesWeb(TemplateView):
+    template_name = 'webacc/general.html'
+    context_object_name = 'objects'
+
+    def get_context_data(self, **kwargs):
+        context = super(RemotesWeb, self).get_context_data(**kwargs)
+        title = 'Cameras and buttons'
+        context.update(
+            title=title,
+            content='Here show and choose some modules'
+        )
+        return context
+
+
+@method_decorator(login_required, name='dispatch')
+class RemotesAllCameras(TemplateView):
+    template_name = 'webacc/all_cameras.html'
+    context_object_name = 'objects'
+
+    def get_context_data(self, **kwargs):
+        context = super(RemotesAllCameras, self).get_context_data(**kwargs)
+        title = 'View all cameras'
+        context.update(
+            title=title,
+            content='Here show and choose some modules'
+        )
+        return context
