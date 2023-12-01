b931f94e3476166319cc3180bdf16aa6ab6705a1
Oleksandr Danylchenko <sanekm2010@gmail.com>
2023-12-01 11:50:57 +0200

    Completely redesign settings and credentials.

--- core/security_RENAME.py
+++ core/security_RENAME.py
@@ -1,85 +1,77 @@
-HOSTNAME = 'HOSTNAME'
-DEV_HOST = 'LOCAL_DEV_HOSTNAME'
+class HostnamesSupported:
+    # Live
+    LIVE_HOSTNAME = 'vps-000000'
+    LIVE_FQDN = 'vps-000000.hostname.org'
+    LIVE_HOSTEMAIL = 'vps-000000@site.com'
+    LIVE_IP = '1.2.3.4'
+    LIVE_WEB = 'vps.site.com'
+    LIVE_SITE_SHORT_NAME = 'core'
+    # Local
+    DEV_HOSTNAME = 'local-pc'
+    DEV_FQDN = '127.0.0.1:8000'
+    DEV_HOSTEMAIL = 'to@site.com'
+    DEV_IP = '127.0.0.1'
+    DEV_WEB = '127.0.0.1'
+    DEV_SITE_SHORT_NAME = 'Local'
 
-VPS = 'HOSTNAME.VPS.net'
+
+class DjangoCreds:
+    SECRET_KEY = 'SECRET'
+    EMAIL_HOST = 'some.mail.com'
+    EMAIL_PORT = '25'
+    EMAIL_HOST_USER = 'no-reply@site.com'
+    EMAIL_HOST_PASSWORD = 'PASS'
+    EMAIL_SUBJECT_PREFIX = '[PREFIX] '
+    DEFAULT_FROM_EMAIL = 'no-reply@site.com'
+    mails = dict(
+        admin='email@email.com',
+    )
+    ADMINS = [
+        ('sanek', 'email@email.com'),
+        ('to_sanek', 'to@site.com'),
+    ]
 
 
-class QueuesCelery:
+class MySQLDatabase:
+    DB_ENGINE = 'django.db.backends.mysql'
+    DB_USER = 'core_db'
+    DB_HOST = 'localhost'
+    DB_PORT = '3306'
+    # Live and DEV versions
+    DB_NAME = 'my_vps'
+    DB_PASSWORD = 'PASSWORD'
+    # Local development use WSL IP!
+    DB_HOST_WSL = '192.168.127.254'
+    DB_HOST_WSL_PASSWD = 'PASSWORD'
+
+
+class RabbitMQCreds:
+    RABBITMQ_USER = 'USER'
+    RABBITMQ_PSWD = 'PASSWORD'
+    BROKER = f'pyamqp://{RABBITMQ_USER}:{RABBITMQ_PSWD}@localhost:5672/layer'
+    RABBITMQ_URL = f'amqp://{RABBITMQ_USER}:{RABBITMQ_PSWD}@localhost:5672/layer'
+
+
+class CeleryCreds:
+    DB_HOST = 'localhost'
+    DB_HOST_Win = '127.0.0.1'
+    DB_HOST_WSL = '192.168.127.254'
+
+    DB_USER = 'celery'
+    DB_PASSWORD = 'PASSWORD'
+
+    BACKEND = 'db+mysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
+    RESULT_BACKEND = 'db+mysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
+
     QUEUE_MAIN = "default"
     # QUEUE_CORE = "core@layer.dq2"
     QUEUE_CORE = "default"
     # QUEUE_REMOTES = "remotes@layer.dq2"
     QUEUE_REMOTES = "default"
 
-
-class Credentials:
-    DEV_HOST = 'LOCAL_DEV_HOSTNAME'
-
-    # SITE = 'WEB_FQDN'
-    SITE = 'LOCAL_DEV_HOSTNAME'
-    SITE_IP = 'REMOTE_IP'
-    SECRET_KEY = 'KEY'
-
-    ENGINE = 'django.db.backends.mysql'
-    NAME = '__DB_NAME__'
-    USER = '__DB_USER__'
-    PASSWORD = '__DB_PASSWORD__'
-    # HOST = 'localhost'
-    HOST = '127.0.0.1'
-    PORT = '3306'
-
-    PASSWORD_LOCAL = '__DB_USER__'
-
-    EMAIL_HOST = 'mail.adm.tools'
-    EMAIL_PORT = '25'
-    EMAIL_HOST_USER = 'no-reply@SITE_FQDN'
-    DEFAULT_FROM_EMAIL = 'no-reply@SITE_FQDN'
-
-    EMAIL_HOST_PASSWORD = '4diGzvPXU$5!zb'
-
-    EMAIL_SUBJECT_PREFIX = '[Trianglesis] '
-
-    # Local
-    HOSTNAME = 'LOCAL_DEV_HOSTNAME'
-    FQDN = '127.0.0.1:8000'
-    HOSTEMAIL = 'to@SITE_FQDN'
-    SITE_SHORT_NAME = 'Local'
-
-    # External
-    # HOSTNAME = 'HOSTNAME'
-    # FQDN = 'HOSTNAME.VPS.net'
-    # WEB = 'WEB_FQDN'
-    # # HOSTEMAIL = 'HOSTNAME@SITE_FQDN'
-    # HOSTEMAIL = 'no-reply@SITE_FQDN'
-    # SITE_SHORT_NAME = 'core'
-
-    # Site domain:
-    LOCAL = 'LOCAL_DEV_HOSTNAME'
-    LIVE = 'HOSTNAME'
-
-    # Future RabbitMQ
-    broker = 'pyamqp://__USER_RABBITMQ__:__PASSWORD__@localhost:5672/layer'
-    # Future MySQL Backend for Celery
-    backend = 'db+mysql://__USER_CELERY__:__PASSWORD__@localhost/__DB_NAME__'
-    # Future MySQL Backend for my_vps
-    result_backend = 'db+mysql://__USER_CELERY__:__PASSWORD__@localhost/__DB_NAME__'
-
-    # wsl_backend='file:///opt/celery/',
-    # wsl_result_backend='file:///opt/celery/',
-    wsl_backend = 'db+mysql://__USER_CELERY__:__PASSWORD__@127.0.0.1/__DB_NAME__'
-    wsl_result_backend = 'db+mysql://__USER_CELERY__:__PASSWORD__@127.0.0.1/__DB_NAME__'
-
-    rabbitmq_url = 'amqp://__USER_RABBITMQ__:__PASSWORD__@localhost:5672/layer'
-    rabbitmq_user = '__USER_RABBITMQ__'
-    rabbitmq_pswd = '__PASSWORD__'
-
-
-mails = dict(
-    admin='EMAIL@EMAIL',
-
-)
-
-ADMINS = [
-    ('USER', 'EMAIL@EMAIL'),
-    ('to_USER', 'to@SITE_FQDN'),
-]
+    WORKERS = ["core@layer",
+               "remotes@layer",
+               ]
+    WSL_WORKERS = ["core@layer",
+                   "remotes@layer",
+                   ]
