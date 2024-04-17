95e077ad28bdf42f3f9c97eaa2d0162e80e62b0c
Oleksandr Danylchenko <oleksandr_danylchenko_cw@bmc.com>
2024-04-17 23:09:51 +0300

    Making main page with last 3 posts

--- main/views.py
+++ main/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 
+from blog.models import Post, Tag
+
 log = logging.getLogger("core")
 
 
@@ -11,6 +13,18 @@ class MainPage(TemplateView):
     template_name = 'main/main_body.html'
     context_object_name = 'objects'
 
-    # TODO: Later add here some photos I want to show with maybe lik to the post.
-    # TODO: Later add random or set of recent or popular posts here
+    def get_context_data(self, **kwargs):
+        context = super(MainPage, self).get_context_data(**kwargs)
+        context.update(
+            debug=False,
+            objects=self.get_queryset(),
+        )
+        return context
+
+    def get_queryset(self):
+        main_data = dict(
+            posts = Post.objects.all(),
+            tags = Tag.objects.all(),
+        )
+        return main_data
 
