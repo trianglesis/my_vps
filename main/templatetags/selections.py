b41ed66073809a9f49e13a24fd2f6448345444a9
Oleksandr Danylchenko <oleksandr_danylchenko_cw@bmc.com>
2024-04-27 16:08:38 +0300

    Update pagination.html with a selector validator to keep URL args.

--- main/templatetags/selections.py
+++ main/templatetags/selections.py
@@ -8,6 +8,30 @@ register = template.Library()
 log = logging.getLogger("core")
 
 
+@register.simple_tag(takes_context=True)
+def selector_validate(context, exclude_key=None, update_context=False):
+    """
+    Looks in context for selector dict.
+    Remove duplicates or empty values.
+    Attach selector to paginator and other sorting or filtering buttons.
+    See pagination.html
+    """
+    selector = context.get('selector')
+    sel_str = ''
+    for sel_k, sel_v in selector.items():
+        if not sel_k == exclude_key:
+            if sel_v:
+                sel = f'{sel_k}={sel_v}&'
+                sel_str += sel
+    if sel_str.endswith('&'):
+        sel_str = sel_str.rstrip(sel_str[-1])
+    if update_context:
+        context[update_context] = sel_str
+        return ''
+    else:
+        return sel_str
+
+
 @register.simple_tag()
 def list_tags():
     return Tag.objects.all()
